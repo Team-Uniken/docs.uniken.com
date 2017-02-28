8bb4757981a7a22f1eeb795de9706e1579d572ba
James Villarrubia <james.villarrubia@gmail.com>
2017-02-28 18:34:56 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+var markdownpdf = require("markdown-pdf")
+
+
+var mdDocs = [
+	"verify.md"
+]
+
+var options = {
+	cssPath: 'custom.css',
+	highlightCssPath: './highlight.css',
+}
+
+var pdfDocs = mdDocs.map(function (d) { return "pdfs/" + d.replace(".md", ".pdf") })
+
+markdownpdf(options).from(mdDocs).to(pdfDocs, function () {
+  pdfDocs.forEach(function (d) { console.log("Created", d) })
+})
+
+*/
+
+var yamp = require('yamp');
+var options = {
+	style:false//'custom.css'
+}
+var pdf = new yamp.renderers.pdf();
+var html = new yamp.renderers.html();
+
+var mdDocs = [
+	"verify.md"
+]
+
+html.renderFile(mdDocs[0], function(err){
+    if (err) return console.log("Error while rendering: "+err);
+    else console.log("HTML Rendering was successful");
+});
+
+pdf.renderFile(mdDocs[0], function(err){
+    if (err) return console.log("Error while rendering: "+err);
+    else console.log("PDF Rendering was successful");
+});
+
+
